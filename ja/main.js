61d5e3da23690629e9de83df75a60c9461acb1b4
yamanekaito <yamanekaito0702@gmail.com>
2020-06-06 14:33:07 +0900

    lesson6

--- ja/main.js
+++ ja/main.js
@@ -1,1 +1,9 @@
-'use strict'
+'use strict';
+
+{
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('modal');
+  const mask = document.getElementById('mask');
+
+}
